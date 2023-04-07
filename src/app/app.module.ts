efab22339b55bee062f918be6f14511b6ec9091e
RahulSainy <kradekidrahul@gmail.com>
2023-04-08 01:04:25 +0530

    new process and all working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CompleteProfileComponent } from './auth/complete-profile/complete-profile.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    DashboardComponent,
+    CompleteProfileComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
